4f50a0fdd279fcb21141f72baffc6be020a16426
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2023-12-05 01:38:03 +0900

    shooting: 背景を画面の大きさに拡大

--- shooting/game.py
+++ shooting/game.py
@@ -1,10 +1,12 @@
 import pygame
+from setting import *
 
 class Game:
     def __init__(self):
         self.screen = pygame.display.get_surface()
         # 背景
-        self.bg_img = pygame.image.load("shooting/assets/img/background/bg.png")
+        self.pre_bg_img = pygame.image.load("shooting/assets/img/background/bg.png") # 画像の読み込み
+        self.bg_img = pygame.transform.scale(self.pre_bg_img, (screen_width, screen_width)) # 画像のサイズを変更
 
     def run(self):
         self.screen.blit(self.bg_img, (0, 0))
