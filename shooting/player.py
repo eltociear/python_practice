1a3cc29f676080057907f5c4fd2c8b47ef360e50
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2023-12-06 02:36:04 +0900

    shooting: 自機表示を画像にする

--- shooting/player.py
+++ shooting/player.py
@@ -6,8 +6,13 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups) # classの継承をした際に、親クラスの変数や関数を使用することができる
 
         # 画像の読み込み
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(RED)
+        self.image_list = []
+        for i in range(3):
+            image = pygame.image.load(f"shooting/assets/img/player/{i}.png") # 　フォルダ内の画像を指定するのにf文字列を使用
+            self.image_list.append(image)
+
+        self.pre_image = self.image_list[0] # 画像の初期化
+        self.image = pygame.transform.scale(self.pre_image, (50, 50))
         self.rect = self.image.get_rect(center=(x, y)) # 画像の中心座標を(x, y)に設定
 
         # 移動
