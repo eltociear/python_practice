c1050f5e4e8a237b7c2ec5baa322ae905e86fa72
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2023-12-07 01:12:13 +0900

    shooting: 弾の挙動をジグザグに

--- shooting/enemy.py
+++ shooting/enemy.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from setting import *
 
 class Enemy(pygame.sprite.Sprite):
@@ -11,10 +12,19 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=(x, y))
 
         # 移動
-        self.direction = pygame.math.Vector2(0, 1) # 最初の数字がx方向、2番目の数字がy方向（デフォルトは0, 0）
+        move_list = [1, -1]
+        self.direction = pygame.math.Vector2(random.choice(move_list), 1) # 最初の数字がx方向、2番目の数字がy方向（デフォルトは0, 0）、move_listからランダムに選択
         self.speed = 1
+        self.timer = 0
 
     def move(self):
+        # ジグザグに折り返しさせる
+        self.timer += 1
+        if self.timer >= 80:
+            self.direction.x *= -1
+            self.timer = 0
+
+        self.rect.x += self.direction.x * self.speed
         self.rect.y += self.direction.y * self.speed
 
     def update(self):
