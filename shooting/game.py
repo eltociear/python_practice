8ea2a88cd69f48d940292527353936ecaf312fd7
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2023-12-07 01:15:40 +0900

    shooting: 敵を複数出現させる

--- shooting/game.py
+++ shooting/game.py
@@ -14,7 +14,7 @@ class Game:
         player = Player(self.player_group, 300, 500)
 
         # 敵
-        enemy = Enemy(self.enemy_group, 300, 100)
+        self.timer = 0 # 敵を作成するタイミングを管理するタイマー
 
         # 背景
         self.pre_bg_img = pygame.image.load("shooting/assets/img/background/bg.png") # 画像の読み込み
@@ -26,6 +26,13 @@ class Game:
         self.player_group = pygame.sprite.GroupSingle() # 1体のプレイヤーを管理するグループ
         self.enemy_group = pygame.sprite.Group() # 敵を管理するグループ
 
+    # 敵を作成する関数
+    def create_enemy(self):
+        self.timer += 1
+        if self.timer >= 50:
+            enemy = Enemy(self.enemy_group, 300, 100)
+            self.timer = 0
+
     def scroll_bg(self):
         self.bg_y = (self.bg_y + self.scroll_speed) % screen_height # self.bg_yが0から599までを繰り返す
         self.screen.blit(self.bg_img, (0, self.bg_y - screen_height)) # y座標は-600から-1に変化
@@ -33,6 +40,8 @@ class Game:
 
     def run(self):
         self.scroll_bg()
+
+        self.create_enemy()
 
         # グループの描画と更新
         self.player_group.draw(self.screen) # Game内のself.screenに描画
