ba73f6b85d54aa29286b311a015e49deccbc92ab
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2023-12-20 16:53:04 +0900

    multi_thread: update main2.py

--- other/multi_thread/main2.py
+++ other/multi_thread/main2.py
@@ -1,11 +1,18 @@
+import time
+import sys
+
 def func_hi():
     for _ in range(100):
         print('hi', end='')
+        sys.stdout.flush() # print()はstdoutに出力するので、flush()でバッファがフラッシュされて強制的に出力する
+        time.sleep(0.2)
     print() # 空文字列を出力することで、改行されないようにする
 
 def func_bye():
     for _ in range(100):
         print('bye', end='')
+        sys.stdout.flush() # print()はstdoutに出力するので、flush()でバッファがフラッシュされて強制的に出力する
+        time.sleep(0.2)
     print() # 空文字列を出力することで、改行されないようにする
 
 func_hi()
