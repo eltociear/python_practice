1eacb30f4616ba3cb1464a15a107ea0f6c9a80f8
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2023-12-24 00:13:12 +0900

    async_await: タスク作成で並行処理

--- other/async_await/script.py
+++ other/async_await/script.py
@@ -1,17 +1,12 @@
 import asyncio
 import time
 
-async def function_1(sec):
-    print(f"{sec}秒待ちます")
-    await asyncio.sleep(sec) # asyncio.sleep()はawaitで待機可能なコルーチン。time.sleep()とは違い、イベントループによって非同期に実行される
-    return f"{sec}秒の待機に成功しました"
-
 async def main(): # async defでコルーチン(処理をある場所で一時中断・再開できる)を定義
     print(f"main開始 {time.strftime('%X')}")
-    result_1 = await function_1(1)
-    result_2 = await function_1(2)
-    print(result_1)
-    print(result_2)
+    task1 = asyncio.create_task(asyncio.sleep(1)) # create_task()で子ルーチンをラップしたタスクを作成 -> これで並行処理が可能
+    task2 = asyncio.create_task(asyncio.sleep(2))
+    await task1 # awaitでタスクが完了するまで待機
+    await task2
     print(f"main終了 {time.strftime('%X')}")
 
 if __name__ == "__main__":
