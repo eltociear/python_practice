21cd4cb90ea0015e055d8b1cef2e56c79942654e
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2024-01-07 22:31:58 +0900

    lifegame: 完成

--- games/lifegame/main.py
+++ games/lifegame/main.py
@@ -32,13 +32,62 @@ def draw_grid(positions):
     for col in range(GLID_WIDTH):
         pygame.draw.line(screen, BLACK, (col * TILE_SIZE, 0), (col * TILE_SIZE, HEIGHT)) # 縦線
 
+def adjust_grid(positions):
+    all_neighbors = set()
+    new_positions = set()
+
+    for position in positions:
+        neighbors = get_neighbors(position)
+        all_neighbors.update(neighbors)
+
+        neighbors = list(filter(lambda x: x in positions, neighbors))
+
+        if len(neighbors) in [2, 3]:
+            new_positions.add(position)
+
+    for position in all_neighbors:
+        neighbors = get_neighbors(position)
+        neighbors = list(filter(lambda x: x in positions, neighbors))
+
+        if len(neighbors) == 3:
+            new_positions.add(position)
+
+    return new_positions
+
+def get_neighbors(pos):
+    x, y = pos
+    neighbors = []
+    for dx in [-1, 0, 1]:
+        if x + dx < 0 or x + dx > GLID_WIDTH:
+            continue
+        for dy in [-1, 0, 1]:
+            if y + dy < 0 or y + dy > GLID_HEIGHT:
+                continue
+            if dx == 0 and dy == 0:
+                continue
+
+            neighbors.append((x + dx, y + dy))
+
+    return neighbors
+
 def main():
     running = True
     playing = False
+    count = 0
+    update_freq = 120
 
     positions = set()
     while running:
         clock.tick(FPS)
+
+        if playing:
+            count += 1
+
+        if count >= update_freq:
+            count = 0
+            positions = adjust_grid(positions)
+
+        pygame.display.set_caption("Playing" if playing else "Paused")
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -60,8 +109,9 @@ def main():
                     playing = not playing
 
                 if event.key == pygame.K_c:
-                    positions.set()
+                    positions = set()
                     playing = False
+                    count = 0
 
                 if event.key == pygame.K_g:
                     positions = gen(random.randrange(4, 10) * GLID_WIDTH) # Gを押すとグリッドがランダムに生成される
