c5afdc7167fa19deee66832cc69f36f5582087f6
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2024-01-07 22:17:44 +0900

    lifegame: ランダム生成関数追加

--- games/lifegame/main.py
+++ games/lifegame/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -16,6 +17,9 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 clock = pygame.time.Clock()
 
+def gen(num):
+    return set([(random.randrange(0, GLID_HEIGHT), random.randrange(0, GLID_WIDTH)) for _ in range(num)])
+
 def draw_grid(positions):
     for position in positions:
         col, row = position
@@ -30,6 +34,7 @@ def draw_grid(positions):
 
 def main():
     running = True
+    playing = False
 
     positions = set()
     while running:
@@ -49,6 +54,17 @@ def main():
                     positions.remove(pos)
                 else:
                     positions.add(pos)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    playing = not playing
+
+                if event.key == pygame.K_c:
+                    positions.set()
+                    playing = False
+
+                if event.key == pygame.K_g:
+                    positions = gen(random.randrange(4, 10) * GLID_WIDTH) # Gを押すとグリッドがランダムに生成される
 
         screen.fill(GRAY)
         draw_grid(positions)
