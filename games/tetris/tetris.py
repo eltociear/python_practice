f2ac7a16c833d6e2933ca365543c75c545dd9fab
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2024-01-21 23:52:11 +0900

    tetris: グリッド描画

--- games/tetris/tetris.py
+++ games/tetris/tetris.py
@@ -5,8 +5,13 @@ class Tetris:
     def __init__(self, app):
         self.app = app
 
+    def draw_grid(self):
+        for x in range(FIELD_W):
+            for y in range(FIELD_H):
+                pg.draw.rect(self.app.screen, 'black', (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
+
     def update(self):
         pass
 
     def draw(self):
-        pass
+        self.draw_grid()
