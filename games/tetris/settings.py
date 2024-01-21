dbfd6ce2e37c26ffc883c2f7d090107a98645737
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2024-01-22 00:10:58 +0900

    tetris: ブロックをランダム表示

--- games/tetris/settings.py
+++ games/tetris/settings.py
@@ -1,8 +1,22 @@
 import pygame as pg
 
+vec = pg.math.Vector2
+
 FPS = 60
 FIELD_COLOR = (48, 39, 32)
 
 TILE_SIZE = 50
 FIELD_SIZE = FIELD_W, FIELD_H = 10, 20
 FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE
+
+INIT_POS_OFFSET = vec(FIELD_SIZE) // 2
+
+TETROMINOES = {
+    'T': [(0, 0), (-1, 0), (1, 0), (0, -1)],
+    'O': [(0, 0), (0, -1), (1, 0), (1, -1)],
+    'J': [(0, 0), (-1, 0), (0, -1), (0, -2)],
+    'L': [(0, 0), (1, 0), (0, -1), (0, -2)],
+    'I': [(0, 0), (0, 1), (0, -1), (0, -2)],
+    'S': [(0, 0), (-1, 0), (0, -1), (1, -1)],
+    'Z': [(0, 0), (1, 0), (0, -1), (-1, -1)]
+}
--- games/tetris/tetromino.py
+++ games/tetris/tetromino.py
@@ -1,20 +1,23 @@
 from settings import *
+import random
 
 class Block(pg.sprite.Sprite):
     def __init__(self, tetromino, pos):
         self.tetromino = tetromino
+        self.pos = vec(pos) + INIT_POS_OFFSET
 
         super().__init__(tetromino.tetris.sprite_group)
         self.image = pg.Surface([TILE_SIZE, TILE_SIZE])
         self.image.fill('orange')
 
         self.rect = self.image.get_rect()
-        self.rect.topleft = pos[0] * TILE_SIZE, pos[1] * TILE_SIZE
+        self.rect.topleft = self.pos * TILE_SIZE
 
 class Tetromino:
     def __init__(self, tetris):
         self.tetris = tetris
-        Block(self, (4, 7))
+        self.shape = random.choice(list(TETROMINOES.keys()))
+        self.blocks = [Block(self, pos) for pos in TETROMINOES[self.shape]]
 
     def update(self):
         pass
