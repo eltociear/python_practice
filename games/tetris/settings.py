9c43c842d698db4348ce78b6f2907ef5d97ff82d
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2024-01-22 16:46:54 +0900

    tetris: ブロックが落下する処理実装

--- games/tetris/settings.py
+++ games/tetris/settings.py
@@ -9,7 +9,8 @@ TILE_SIZE = 50
 FIELD_SIZE = FIELD_W, FIELD_H = 10, 20
 FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE
 
-INIT_POS_OFFSET = vec(FIELD_SIZE) // 2
+INIT_POS_OFFSET = vec(FIELD_W // 2 - 1, 0)
+MOVE_DIRECTIONS = {'left': vec(-1, 0), 'right': vec(1, 0), 'down': vec(0, 1)}
 
 TETROMINOES = {
     'T': [(0, 0), (-1, 0), (1, 0), (0, -1)],
--- games/tetris/tetromino.py
+++ games/tetris/tetromino.py
@@ -9,9 +9,13 @@ class Block(pg.sprite.Sprite):
         super().__init__(tetromino.tetris.sprite_group)
         self.image = pg.Surface([TILE_SIZE, TILE_SIZE])
         self.image.fill('orange')
-
         self.rect = self.image.get_rect()
+
+    def set_rect_pos(self):
         self.rect.topleft = self.pos * TILE_SIZE
+
+    def update(self):
+        self.set_rect_pos()
 
 class Tetromino:
     def __init__(self, tetris):
@@ -19,5 +23,11 @@ class Tetromino:
         self.shape = random.choice(list(TETROMINOES.keys()))
         self.blocks = [Block(self, pos) for pos in TETROMINOES[self.shape]]
 
+    def move(self, direction):
+        move_direction = MOVE_DIRECTIONS[direction]
+        for block in self.blocks:
+            block.pos += move_direction
+
     def update(self):
-        pass
+        self.move(direction='down')
+        pg.time.wait(200)
