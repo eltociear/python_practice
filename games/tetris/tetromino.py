01f9436d52d99880d3cc841785332876486ef027
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2024-01-22 17:00:50 +0900

    tetris: ブロックの衝突判定追加

--- games/tetris/tetromino.py
+++ games/tetris/tetromino.py
@@ -17,16 +17,29 @@ class Block(pg.sprite.Sprite):
     def update(self):
         self.set_rect_pos()
 
+    def is_collide(self, pos):
+        x, y = int(pos.x), int(pos.y)
+        if 0 <= x < FIELD_W and y < FIELD_H:
+            return False
+        return True
+
 class Tetromino:
     def __init__(self, tetris):
         self.tetris = tetris
         self.shape = random.choice(list(TETROMINOES.keys()))
         self.blocks = [Block(self, pos) for pos in TETROMINOES[self.shape]]
 
+    def is_collide(self, block_positions):
+        return any(map(Block.is_collide, self.blocks, block_positions))
+
     def move(self, direction):
         move_direction = MOVE_DIRECTIONS[direction]
-        for block in self.blocks:
-            block.pos += move_direction
+        new_block_positions = [block.pos + move_direction for block in self.blocks]
+        is_collide = self.is_collide(new_block_positions)
+
+        if not is_collide:
+            for block in self.blocks:
+                block.pos += move_direction
 
     def update(self):
         self.move(direction='down')
